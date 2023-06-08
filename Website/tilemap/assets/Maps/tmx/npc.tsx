f75bedc24de9a671d6eb92c1bbd30231315de9d7
Pubenu <135981762+Pubenu@users.noreply.github.com>
2023-06-08 09:01:05 -0400

    Delete Website directory

--- Website/tilemap/assets/Maps/tmx/npc.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="Untitled" tilewidth="8" tileheight="8" tilecount="96" columns="8">
- <image source="../../tiled/tileset/npcs.png" trans="ffffff" width="64" height="96"/>
-</tileset>
--- Website/tilemap/assets/tiled/BigSet.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="BigSet" tilewidth="8" tileheight="8" tilecount="3844" columns="62">
- <image source="tileset/BigSet.png" trans="ffffff" width="500" height="500"/>
-</tileset>
